96dd6542a6ca160d661fd71db3363aad2fecec90
James Sutherland <jrsutherland78@googlemail.com>
2018-11-29 14:07:50 +0000

    chore: build production version

---
+++ build/precache-manifest.f62856c901aa42eeb4861ba8c8c6d02c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "914f3e97619203355de7",
+    "url": "/static/css/main.27573540.chunk.css"
+  },
+  {
+    "revision": "914f3e97619203355de7",
+    "url": "/static/js/main.914f3e97.chunk.js"
+  },
+  {
+    "revision": "3a134f815065bb42764d",
+    "url": "/static/js/1.3a134f81.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "a17f81c3ae882309507447f343d4f172",
+    "url": "/index.html"
+  }
+];
